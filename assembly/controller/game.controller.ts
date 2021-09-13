c2c781b5311b468067cb375665ec825546c22134
phamr39 <pha.mr3998@gmail.com>
2021-09-13 18:13:26 +0700

    update cedeo

--- assembly/controller/game.controller.ts
+++ assembly/controller/game.controller.ts
@@ -1,3 +1,50 @@
 import { Game } from "../model/game.model";
+import { Token } from "../model/token.model";
+import { Space } from "../model/space.model";
+import { SpaceStorage } from "../storage/space.storage";
+import { Context } from "near-sdk-core";
+import { TokenStorage } from "../storage/token.storage";
+import { GameStorage } from "../storage/game.storage";
 
-export function gm_register(name: String, symbol: String, icon: String | null, space: String, token: String): void {}
+export function gm_register(name: String, symbol: String, icon: String | null, space: String, token: String): String {
+    if (GameStorage.contain(name)) {
+        return 'This game already registered, please chose another name!';
+    }
+    let isSpace: Space | null = SpaceStorage.get(Context.sender, space);
+    let isToken: Token | null = TokenStorage.get(Context.sender, token);
+    let reg_game: Game;
+    if (isSpace && isToken) {
+        reg_game = new Game(name, symbol, icon, isSpace);
+        reg_game.update_token(isToken);
+        reg_game.register();
+    } else if (!isSpace && isToken) {
+        return 'Space Not Found!';
+    } else if (isSpace && !isToken) {
+        return 'Token Not Found!';
+    }
+    return 'Register Done!';
+}
+
+export function gm_unregisters(): Game[] | null {
+    const ownerId = Context.sender;
+    return GameStorage.deletes(ownerId);
+}
+
+export function gm_unregister(name: String): Game | null {
+    const ownerId = Context.sender;
+    return GameStorage.delete(ownerId, ownerId);
+}
+
+export function gm_update(name: String, symbol: String, icon: String | null): bool {
+    const ownerId = Context.sender;
+    const space = SpaceStorage.get(ownerId, name);
+    if (space == null) {
+        return false;
+    }
+    
+    space.update_symbol(symbol);
+    space.update_icon(icon);
+    space.save();
+
+    return true;
+}
--- assembly/model/game.model.ts
+++ assembly/model/game.model.ts
@@ -1,14 +1,38 @@
+import { GameStorage } from "../storage/game.storage";
 import { Space } from "./space.model";
 import { Token } from "./token.model";
 
 @nearBindgen
 export class Game {
     public token: Token;
-    constructor(public name: String, public symbol: String, public icon: String | null, public space: Space) {}
+    constructor(public name: String, public symbol: String | null, public icon: String | null, public space: Space) {}
+
+    register() {
+        this.save();
+    }
 
     update_token(token: Token) {
         if (!this.token.compare(token)) {
             this.token = token;
         }
+        this.save();
+    }
+
+    update_symbol(symbol: String) {
+        if (symbol != this.symbol) {
+            this.symbol = symbol;
+        }
+        this.save();
+    }
+
+    update_icon(icon: String | null) {
+        if (icon != this.icon) {
+            this.icon = icon;
+        }
+        this.save();
+    }
+
+    save() {
+        GameStorage.set(this.space.name, this);
     }
 }
